e5ac547dc4b5b0a69d37c8b3333ce928939b504f
Alpha Olomi <alphaolomi@gmail.com>
2021-02-25 09:57:16 +0300

    --wip-- [skip ci]

---
+++ common/SvgIcon/index.js
@@ -0,0 +1,6 @@
+
+const SvgIcon = ({ src, width, height }) => (
+  <img src={`/img/svg/${src}`} alt={src} with={width} height={height} />
+);
+
+export default SvgIcon;
---
+++ pages/_home.js
@@ -0,0 +1,67 @@
+
+
+import IntroContent from "../content/IntroContent.json";
+import MiddleBlockContent from "../content/MiddleBlockContent.json";
+import AboutContent from "../content/AboutContent.json";
+import MissionContent from "../content/MissionContent.json";
+import ProductContent from "../content/ProductContent.json";
+import ContactContent from "../content/ContactContent.json";
+
+
+import ContactFrom from "../components/ContactForm";
+import ContentBlock from "../components/ContentBlock";
+import MiddleBlock from "../components/MiddleBlock";
+import Container from "../common/Container";
+import ScrollToTop from "../common/ScrollToTop";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        type="right"
+        first="true"
+        title={IntroContent.title}
+        content={IntroContent.text}
+        button={IntroContent.button}
+        icon="developer.svg"
+        id="intro"
+      />
+      <MiddleBlock
+        title={MiddleBlockContent.title}
+        content={MiddleBlockContent.text}
+        button={MiddleBlockContent.button}
+      />
+      <ContentBlock
+        type="left"
+        title={AboutContent.title}
+        content={AboutContent.text}
+        section={AboutContent.section}
+        icon="graphs.svg"
+        id="about"
+      />
+      <ContentBlock
+        type="right"
+        title={MissionContent.title}
+        content={MissionContent.text}
+        icon="product-launch.svg"
+        id="mission"
+      />
+
+      <ContentBlock
+        type="left"
+        title={ProductContent.title}
+        content={ProductContent.text}
+        icon="waving.svg"
+        id="product"
+      />
+      <ContactFrom
+        title={ContactContent.title}
+        content={ContactContent.text}
+        id="contact"
+      />
+    </Container>
+  );
+};
+
+export default HomePage;
